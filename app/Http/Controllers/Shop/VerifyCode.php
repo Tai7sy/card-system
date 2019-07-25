b767c07737e30f994600baf183ba89cb44fd0e69
windy <12840640+Tai7sy@users.noreply.github.com>
2019-07-25 16:30:26 +0800

    2.93

--- app/Http/Controllers/Shop/VerifyCode.php
+++ app/Http/Controllers/Shop/VerifyCode.php
@@ -1,2 +1,2 @@
 <?php
-namespace App\Http\Controllers\Shop; use App\Library\Response; use App\Library\Geetest; use Illuminate\Http\Request; use App\Http\Controllers\Controller; class VerifyCode extends Controller { function getVerify() { $sp1d2d7f = array('driver' => 'geetest', 'geetest' => Geetest\API::get()); return Response::success($sp1d2d7f); } }
+namespace App\Http\Controllers\Shop; use App\Library\Response; use App\Library\Geetest; use Illuminate\Http\Request; use App\Http\Controllers\Controller; class VerifyCode extends Controller { function getVerify() { $spf81742 = array('driver' => 'geetest', 'geetest' => Geetest\API::get()); return Response::success($spf81742); } }
