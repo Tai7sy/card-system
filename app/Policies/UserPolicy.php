d340d47e3c23f53df6513e8fc00019cc55af53a1
windy <12840640+Tai7sy@users.noreply.github.com>
2020-01-11 21:44:03 +0800

    Snapshot 3.0

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -1,2 +1,2 @@
 <?php
-namespace App\Policies; use App\User; use Illuminate\Auth\Access\HandlesAuthorization; class UserPolicy { use HandlesAuthorization; public function __construct() { } public function admin($spf3d3c9) { } public function merchant($spf3d3c9) { } public function before($spf3d3c9, $spaf2848) { return true; } }
+namespace App\Policies; use App\User; use Illuminate\Auth\Access\HandlesAuthorization; class UserPolicy { use HandlesAuthorization; public function __construct() { } public function admin($sp264a55) { } public function merchant($sp264a55) { } public function before($sp264a55, $sp7d9a16) { return true; } }
--- database/migrations/2019_06_18_112211_add_manual_products.php
+++ database/migrations/2019_06_18_112211_add_manual_products.php
@@ -1,6 +1,6 @@
 <?php
-use Illuminate\Support\Facades\Schema; use Illuminate\Database\Schema\Blueprint; use Illuminate\Database\Migrations\Migration; class AddManualProducts extends Migration { public function up() { if (!Schema::hasColumn('products', 'fields')) { Schema::table('products', function (Blueprint $sp6e715d) { $sp6e715d->text('fields')->nullable()->after('instructions'); $sp6e715d->tinyInteger('delivery')->default(\App\Product::DELIVERY_AUTO)->after('fee_type'); }); } if (!Schema::hasColumn('orders', 'contact_ext')) { DB::unprepared('
+use Illuminate\Support\Facades\Schema; use Illuminate\Database\Schema\Blueprint; use Illuminate\Database\Migrations\Migration; class AddManualProducts extends Migration { public function up() { if (!Schema::hasColumn('products', 'fields')) { Schema::table('products', function (Blueprint $sp2bac3d) { $sp2bac3d->text('fields')->nullable()->after('instructions'); $sp2bac3d->tinyInteger('delivery')->default(\App\Product::DELIVERY_AUTO)->after('fee_type'); }); } if (!Schema::hasColumn('orders', 'contact_ext')) { DB::unprepared('
 ALTER TABLE `orders`
 CHANGE COLUMN `email` `contact`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL AFTER `customer`,
 CHANGE COLUMN `email_sent` `send_status`  tinyint(4) NOT NULL DEFAULT 0 AFTER `contact`;
-            '); Schema::table('orders', function (Blueprint $sp6e715d) { $sp6e715d->text('contact_ext')->nullable()->after('contact'); }); } } public function down() { } }
+            '); Schema::table('orders', function (Blueprint $sp2bac3d) { $sp2bac3d->text('contact_ext')->nullable()->after('contact'); }); } } public function down() { } }
