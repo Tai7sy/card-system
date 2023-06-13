4b1acfc2b882eafdba9260b3dccfb2a6e149c2a0
windy <19060@qq.com>
2023-06-13 23:44:26 +0800

    config file for theme

--- resources/views/shop_theme/Test/config.php
+++ resources/views/shop_theme/Test/config.php
@@ -1,20 +1,49 @@
 <?php
 
 return [
-    'description' => '经典主题',
+    'description' => '测试模板',
     'options' => [
+        'theme' => [
+            'label' => '风格',
+            'type' => 'radio',
+            'values' => [
+                'light' => [
+                    'label' => '浅色主题'
+                ],
+                'dark' => [
+                    'label' => '深色主题'
+                ]
+            ],
+            'value' => 'light'
+        ],
         'list_type' => [
             'label' => '商品显示方式',
             'type' => 'select',
+            'hidden' => true,
             'values' => [
-                'dropdown' => [
-                    'label' => '下拉式'
-                ],
-                'button' => [
-                    'label' => '按钮式'
+                'list' => [
+                    'label' => '列表式'
                 ]
             ],
-            'value' => 'dropdown'
+            'value' => 'list'
+        ],
+        'test' => [
+            'label' => '测试text',
+            'type' => 'text',
+            'inputType' => 'text',
+            'size' => 16,
+            'value' => '默认值'
+        ],
+        'test_textarea' => [
+            'label' => '测试textarea',
+            'type' => 'textarea',
+            'rows' => 5,
+            'value' => '默认值'
+        ],
+        'test_checkbox' => [
+            'label' => '测试checkbox',
+            'type' => 'checkbox',
+            'value' => 1
         ]
     ]
 ];
