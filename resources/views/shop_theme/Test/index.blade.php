e0bf1b08dd0ea981a155b55055300a708ef8ce07
windy <19060@qq.com>
2023-07-12 00:00:58 +0800

    fix index path

--- resources/views/shop_theme/Test/index.blade.php
+++ resources/views/shop_theme/Test/index.blade.php
@@ -2,7 +2,7 @@
 $version = '1.0';
 ?>
 @if(@preg_match('/(iPhone|iPod|Android|ios|SymbianOS|Windows Phone)/i', $_SERVER['HTTP_USER_AGENT']))
-    @include('shop_theme.ACG.mobile')
+    @include('shop_theme.Test.mobile')
 @else
-    @include('shop_theme.ACG.pc')
+    @include('shop_theme.Test.pc')
 @endif
