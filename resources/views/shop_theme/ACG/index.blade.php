70959f67a90dc3ff1689cbbd805224588bf26f4c
windy <12840640+Tai7sy@users.noreply.github.com>
2024-08-16 18:52:11 +0800

    New theme

---
+++ resources/views/shop_theme/ACG/index.blade.php
@@ -0,0 +1,8 @@
+<?php
+$version = '2.3';
+?>
+@if(@preg_match('/(iPhone|iPod|Android|ios|SymbianOS|Windows Phone)/i', $_SERVER['HTTP_USER_AGENT']))
+    @include('shop_theme.Classic.mobile')
+@else
+    @include('shop_theme.Classic.pc')
+@endif
