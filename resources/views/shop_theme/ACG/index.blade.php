851b85e3a001c5a8fe5e5b3cb25f0c65920adb0b
windy <12840640+Tai7sy@users.noreply.github.com>
2024-08-17 00:08:25 +0800

    Add new theme template

--- resources/views/shop_theme/ACG/index.blade.php
+++ resources/views/shop_theme/ACG/index.blade.php
@@ -2,7 +2,7 @@
 $version = '2.3';
 ?>
 @if(@preg_match('/(iPhone|iPod|Android|ios|SymbianOS|Windows Phone)/i', $_SERVER['HTTP_USER_AGENT']))
-    @include('shop_theme.Classic.mobile')
+    @include('shop_theme.ACG.mobile')
 @else
-    @include('shop_theme.Classic.pc')
+    @include('shop_theme.ACG.pc')
 @endif
